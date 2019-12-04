387ee9daf463ce6a7c448223908f7d58d3dcb22e
ginnikhanna <ginnikhanna27@gmail.com>
2019-12-04 18:00:12 +0100

    Updated binomial distribution with graphs in bokeh

--- Probability/distributions.py
+++ Probability/distributions.py
@@ -1,25 +1,33 @@
 from enum import Enum
+from scipy.special import comb
 from collections import namedtuple
 
 
 
 class Distribution(Enum):
     bernoulli = 1
+    binomial = 2
 
 
 
-distribution_bernoulli_parameters = namedtuple('BernoulliDistribution', ['probability_of_success', 'random_variable_value'])
+BernoulliDistribution = namedtuple('BernoulliDistribution', ['probability_of_success', 'random_variable_value'])
+BinomialDistribution = namedtuple('BinomialDistribution', ['number_of_trials', 'number_of_success_trials', 'probability_of_success'])
 
-
-def get_probability_of_occurence(parameters:distribution_bernoulli_parameters):
+def get_probability_of_occurence(distribution:Distribution, parameters :(BernoulliDistribution, BinomialDistribution)):
 
     ''' Let us say there is a random variable X which can only take two values, either 0 or 1.
     The probability distribution of such a random variable is modelled using the Bernoulli distribution with probabily p and is written as
 
     P(X = x,p) = p^x(1-p)^^1-x) for x = 0, 1'''
 
+    if distribution == Distribution.bernoulli:
+        pdf = pow(parameters.probability_of_success, parameters.random_variable_value) * pow((1 - parameters.probability_of_success), (1-parameters.random_variable_value))
 
-    pdf = pow(parameters.probability_of_success, parameters.random_variable_value) * pow((1 - parameters.probability_of_success), (1-parameters.random_variable_value))
+        return pdf
 
+    if distribution == Distribution.binomial:
+        pdf = comb(parameters.number_of_trials, parameters.number_of_success_trials) * \
+              pow(parameters.probability_of_success, parameters.number_of_success_trials) * \
+              pow((1 - parameters.probability_of_success), parameters.number_of_trials - parameters.number_of_success_trials)
 
-    return pdf
+        return pdf
--- Probability/plotdistributions.py
+++ Probability/plotdistributions.py
@@ -5,7 +5,9 @@ from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource
 from bokeh.io import show
 import pandas as pd
+import numpy as np
 
+#from collections import List
 
 def plot_distribution_bernoulli(p_success, p_failure):
 
@@ -34,5 +36,42 @@ def plot_distribution_bernoulli(p_success, p_failure):
 
     p.xaxis.ticker = [0.25, 0.75]
     p.xaxis.major_label_overrides = {0.25: '0', 0.75: '1'}
+
+    return p
+
+
+
+def plot_distribution_binomial(n, p_list):
+
+    p = figure(plot_width=600,
+               plot_height=600,
+               title='Binomial Distribution',
+               x_axis_label='Random Variable Value (X)',
+               y_axis_label='Probability Density Function P(X = x)')
+
+    left_edges_list = np.arange(0.0, len(n) - 1, 0.5)
+    right_edges_list = np.arange(0.5, len(n) + 0.5, 0.5)
+    x_ticks = n
+
+    df_pdf = pd.DataFrame(list(zip(n, p_list, left_edges_list, right_edges_list, n)),
+                      columns=['Number of Successeful Trials',
+                               'P(Event)',
+                               'edges_left',
+                               'edges_right',
+                               'x_ticks'])
+
+
+    df_pdf_src = ColumnDataSource(df_pdf)
+    p.quad(source=df_pdf_src,
+           bottom=0, top='P(Event)',
+           left='edges_left',
+           right='edges_right',
+           fill_color='red', line_color='black')
+
+  #  p.xaxis.ticker = [0.25, 0.75, 1.25, 1.75, 2.25, 2.75]
+    x_ticks = np.arange(0.25, len(n)/2 +  0.25, 0.5)
+
+    p.axis.ticker = x_ticks
+    p.axis.major_label_overrides = {key : str(value) for (key, value) in zip(x_ticks, n)}
 
     return p
