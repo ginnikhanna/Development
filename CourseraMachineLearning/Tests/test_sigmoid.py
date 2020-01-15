21f6ace072cb86f4fe57e71b43995d2e94674416
ginnikhanna <ginnikhanna27@gmail.com>
2020-01-15 18:06:13 +0100

    Sigmoid function corrected

--- CourseraMachineLearning/Tests/test_sigmoid.py
+++ CourseraMachineLearning/Tests/test_sigmoid.py
@@ -49,7 +49,7 @@ class Test(unittest.TestCase):
         """
 
         input_scalar = math.inf
-        given_res_scalar = 0.0
+        given_res_scalar = 1.0
         test_res_scalar = sigmoid(input_scalar)
         self.assertEqual(given_res_scalar, test_res_scalar)
 
@@ -60,7 +60,7 @@ class Test(unittest.TestCase):
         """
 
         input_vector = np.ones((3, 1)) * math.inf
-        given_res_vector = np.zeros_like((input_vector))
+        given_res_vector = np.ones_like((input_vector))
         test_res_vector = sigmoid(input_vector)
         self.assertEqual(given_res_vector.all(), test_res_vector.all())
 
@@ -71,7 +71,7 @@ class Test(unittest.TestCase):
         """
 
         input_matrix = np.ones((3, 3)) * math.inf
-        given_res_matrix = np.zeros_like((input_matrix))
+        given_res_matrix = np.ones_like((input_matrix))
         test_res_matrix = sigmoid(input_matrix)
         self.assertEqual(given_res_matrix.all(), test_res_matrix.all())
 
@@ -82,7 +82,7 @@ class Test(unittest.TestCase):
         """
 
         input_scalar = -math.inf
-        given_res_scalar = 1.0
+        given_res_scalar = 0.0
         test_res_scalar = sigmoid(input_scalar)
         self.assertEqual(given_res_scalar, test_res_scalar)
 
@@ -93,7 +93,7 @@ class Test(unittest.TestCase):
         """
 
         input_vector = np.ones((3, 1)) * (-math.inf)
-        given_res_vector = np.ones_like((input_vector))
+        given_res_vector = np.zeros_like((input_vector))
         test_res_vector = sigmoid(input_vector)
         self.assertEqual(given_res_vector.all(), test_res_vector.all())
 
@@ -104,7 +104,7 @@ class Test(unittest.TestCase):
         """
 
         input_matrix = np.ones((3, 1)) * (-math.inf)
-        given_res_matrix = np.ones_like((input_matrix))
+        given_res_matrix = np.zeros_like((input_matrix))
         test_res_matrix = sigmoid(input_matrix)
         self.assertEqual(given_res_matrix.all(), test_res_matrix.all())
 
--- CourseraMachineLearning/Utility/logisticregression.py
+++ CourseraMachineLearning/Utility/logisticregression.py
@@ -1,11 +1,12 @@
 import numpy as np
+import scipy.optimize as so
 
 def sigmoid(z):
 
-    sig = 1/(1 + np.exp(z))
+    sig = 1/(1 + np.exp(-z))
     return sig
 
-def compute_cost(X : np.ndarray  , theta : np.ndarray, y:np.ndarray) -> float:
+def compute_cost(theta : np.ndarray, X : np.ndarray , y:np.ndarray) -> float:
     ''' X : np.array with  M x N dimensions
         theta : np.array with M x 1 dimensions
         y : np.array with  1 x N dimensions
@@ -14,9 +15,10 @@ def compute_cost(X : np.ndarray  , theta : np.ndarray, y:np.ndarray) -> float:
     '''
 
     J_theta = np.mean(-y * np.log(sigmoid(theta.transpose().dot(X))) - (1-y) * np.log(1 - sigmoid(theta.transpose().dot(X))))
+
     return J_theta
 
-def compute_gradients(X : np.ndarray  , theta : np.ndarray, y:np.ndarray) -> np.ndarray:
+def compute_gradients(theta : np.ndarray, X : np.ndarray , y:np.ndarray) -> np.ndarray:
     ''' X : np.array with  M x N dimensions
         theta : np.array with M x 1 dimensions
         y : np.array with  1 x N dimensions
@@ -24,7 +26,18 @@ def compute_gradients(X : np.ndarray  , theta : np.ndarray, y:np.ndarray) -> np.
         N : number of training samples
     '''
 
+    print(theta)
     gradients = (sigmoid(theta.transpose().dot(X)) - y).dot(X.transpose())/len(y)
-
-
     return gradients
+
+
+def minimize_cost_and_find_theta(initial_theta, X, y):
+
+    # Advanced minimizing algorithm
+    result = so.minimize(fun =compute_cost,
+                         x0=initial_theta,
+                         args = (X,y),
+                         jac = compute_gradients)
+
+    return result
+
