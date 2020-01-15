2e66e5ce7129aa5164da7677049be641df3afb3d
ginnikhanna <ginnikhanna27@gmail.com>
2020-01-15 16:46:21 +0100

    Cost and gradient computed

--- CourseraMachineLearning/LogisticRegression/student_admission_logisticregression.py
+++ CourseraMachineLearning/LogisticRegression/student_admission_logisticregression.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 from CourseraMachineLearning.Utility.logisticregression import sigmoid
+from CourseraMachineLearning.Utility import logisticregression
 
-
-# LOAD DATA
+#Load data
 df = pd.read_csv('ex2data1.csv')
 X = np.array((df['Score_Test_1'].to_numpy(), df['Score_Test_2'].to_numpy()))
 
@@ -18,12 +18,11 @@ no_indices = np.where((df['Admission'])== 0)[0]
 
 df_yes = df.drop(no_indices)
 df_no = df.drop(yes_indices)
-
 assert len(df_yes) + len(df_no) == len(df)
-
 y = np.array(df['Admission'])
 number_of_samples = len(y)
 
+# Plot data
 plt.figure(1)
 plt.scatter(df_yes['Score_Test_1'], df_yes['Score_Test_2'], marker = 'o', label = 'Admitted')
 plt.scatter(df_no['Score_Test_1'], df_no['Score_Test_2'], marker = 'x', label = 'Not Admitted')
@@ -31,3 +30,14 @@ plt.xlabel('Scores Test 1')
 plt.ylabel('Score Test 2')
 plt.legend()
 plt.show()
+
+# Prepare data to calculate cost function
+ones = np.ones((1,number_of_samples))
+X = np.vstack((ones, X))
+
+initial_theta = np.zeros((3,1))
+cost = logisticregression.compute_cost(X, initial_theta, y)
+gradients = logisticregression.compute_gradients(X, initial_theta, y)
+
+print(f'Cost : {cost}')
+print(f'Gradients: {gradients}')
