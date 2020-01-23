e9ecc73bf1a1556b4896125fd443a81676d323f4
ginnikhanna <ginnikhanna27@gmail.com>
2020-01-23 16:00:45 +0100

    added one_vs_all_classifier

--- CourseraMachineLearning/Utility/logisticregression.py
+++ CourseraMachineLearning/Utility/logisticregression.py
@@ -219,3 +219,47 @@ def construct_matrix_with_mapped_features(X: np.ndarray, degree) -> np.ndarray:
             X_out = np.vstack((X_out, pow(X[0], (i - j)) * pow(X[1], j)))
 
     return X_out
+
+def one_vs_all_classifier(X: np.ndarray, y:np.ndarray, num_labels:int, lambda_for_regularization:float, optimization_algo:str ) -> np.ndarray:
+    '''
+    :param X: M x N matrix
+    :param y: 1 x N vector
+    :param theta : M x 1 vector
+    :param lambda_for_regularization
+    :param optimization_algo : 'minimize', 'fmin_cg'
+    M : number of parameteres
+    N : number of training samples
+    :return: optimized_theta with dimensions
+    '''
+
+    ones = np.ones((1, X.shape[0]))
+    X = X.transpose()
+    X = np.vstack((ones, X))
+    y = y.flatten()
+
+    theta = np.zeros((X.shape[0]))
+    optimized_theta = np.zeros_like(theta)
+    all_theta = np.zeros((X.shape[0], num_labels))
+    cost = np.zeros((num_labels))
+
+    for label in np.arange(1, 11, 1):
+        y_training = (y == label).astype(int)
+        optimized_parameters = logisticregression.minimize_cost_and_find_theta_with_regularization(theta, X, y_training, lambda_for_regularization, optimization_algo)
+        all_theta[:,label-1] = optimized_parameters
+
+    return all_theta
+
+def predict_outcome_for_digit_dataset(X : np.ndarray, theta : np.ndarray):
+    '''
+
+    :param X: M x N matrix
+    :param theta: M x number_of_digits
+    :return: prediction : 1 x N
+    '''
+    ones = np.ones((1, X.shape[0]))
+    X = X.transpose()
+    X = np.vstack((ones, X))
+    prediction = X.transpose().dot(theta)
+    prediction = np.argmax(prediction, axis = 1) + 1
+
+    return prediction
