08ce6edeffc01e2d10e4e155844063a5435cc9ce
ginnikhanna <ginnikhanna27@gmail.com>
2020-01-23 14:55:30 +0100

    one_vs_all classification implemented

--- CourseraMachineLearning/NeuralNetworks/multiclass_classification.py
+++ CourseraMachineLearning/NeuralNetworks/multiclass_classification.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 from CourseraMachineLearning.Utility import neuralnetworks
 from CourseraMachineLearning.Utility import logisticregression
+from CourseraMachineLearning.Utility.logisticregression import OptimizationAlgo
 # Load Data
 data = scipy.io.loadmat('ex3data1.mat')
 print('End')
@@ -19,17 +20,22 @@ theta = np.array([-2, -1, 1, 2])
 X_t = np.arange(1, 16).reshape(3,5)/10
 y = np.array([1,0,1,0,1])
 ones = np.array(np.ones(5))
-
 X = np.vstack((ones, X_t))
-cost = logisticregression.compute_cost_with_regularization(theta, X, y, lambda_for_regularization=3)
-gradients = logisticregression.compute_gradients_with_regularization(theta, X, y, lambda_for_regularization=3)
 
-gradients = logisticregression.minimize_cost_and_find_theta_with_regularization(theta, X, y, lambda_for_regularization=3)
+lambda_for_regularization  = 3
+
+cost = logisticregression.compute_cost_with_regularization(theta, X, y, lambda_for_regularization)
+gradients = logisticregression.compute_gradients_with_regularization(theta, X, y, lambda_for_regularization)
+#gradients = logisticregression.minimize_cost_and_find_theta_with_regularization(theta, X, y, lambda_for_regularization, OptimizationAlgo.MINIMIZE)
 
 print(f'Cost : {cost}')
 print(f'Gradients : {gradients}')
-#
-num_labels = 10
-neuralnetworks.one_vs_all(X_training, y_training, num_labels)
 
-#plt.show()
+num_labels = 10
+lambda_for_regularization  = 0.1
+all_theta = neuralnetworks.one_vs_all(X_training, y_training, num_labels, lambda_for_regularization, OptimizationAlgo.FMIN_CG)
+
+prediction_on_training_set = neuralnetworks.predict_outcome_for_digit_dataset(X_training, all_theta)
+accuracy = logisticregression.get_accuracy(prediction_on_training_set, y_training.flatten())
+print(f'Accuracy on the training set is {accuracy}')
+
