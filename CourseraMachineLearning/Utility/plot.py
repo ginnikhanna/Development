3ce0f16ac0fda440e70eefe9eebd3158e3a6c7a4
ginnikhanna <ginnikhanna27@gmail.com>
2020-01-24 17:22:19 +0100

    Plotting added for single digit too

--- CourseraMachineLearning/Utility/plot.py
+++ CourseraMachineLearning/Utility/plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from CourseraMachineLearning.Utility import logisticregression
 
-def display_data(X : np.ndarray, number_of_images_to_plot:int) -> plt.figure():
+def display_data(X : np.ndarray, number_of_images_to_plot:int, fig_number:int) -> plt.figure():
     '''
     Reads a NxM matrix, makes a reshaped matrix and plots the individual images on a
     square grid depending on how many images have to be plotted
@@ -18,11 +18,18 @@ def display_data(X : np.ndarray, number_of_images_to_plot:int) -> plt.figure():
     columns = rows
 
     number_of_pixels = int(np.sqrt(X.shape[1]))
-    fig = plt.figure(1 , figsize=(rows, columns))
-    for index, value in enumerate(random_images):
-        X_reshaped = X[value].reshape((number_of_pixels,number_of_pixels))
-        fig.add_subplot(rows, columns, index + 1)
+
+    if number_of_images_to_plot == 1:
+        fig = plt.figure(fig_number, figsize=(3, 3))
+        X_reshaped = X.reshape((number_of_pixels,number_of_pixels))
         plt.imshow(X_reshaped)
+
+    elif number_of_images_to_plot > 1:
+        fig = plt.figure(fig_number, figsize=(rows, columns))
+        for index, value in enumerate(random_images):
+            X_reshaped = X[value].reshape((number_of_pixels,number_of_pixels))
+            fig.add_subplot(rows, columns, index + 1)
+            plt.imshow(X_reshaped)
 
     return fig
 
