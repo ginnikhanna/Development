b39f0640d13b5d2073bc8dec55e0f1899c486c42
ginnikhanna <ginnikhanna27@gmail.com>
2020-02-04 17:38:24 +0100

    Code added for polynomial regression

--- CourseraMachineLearning/LinearRegression/water_through_dam_regularizedlinearregression.py
+++ CourseraMachineLearning/LinearRegression/water_through_dam_regularizedlinearregression.py
@@ -12,8 +12,8 @@ from CourseraMachineLearning.Utility.linearregression import OptimizationAlgo
 #Load Data
 data = scipy.io.loadmat('ex5data1.mat')
 
-X_training = data['X']
-X_val = data['Xval']
+X_training_raw = data['X']
+X_val_raw = data['Xval']
 X_test = data['Xtest']
 
 y_training = data['y']
@@ -23,19 +23,19 @@ y_test = data['ytest']
 # Plot Data
 
 plt.figure(1)
-plt.scatter(X_training, y_training)
+plt.scatter(X_training_raw, y_training)
 plt.xlabel('Change in water level')
 plt.ylabel('Water flowing through dam')
 plt.grid()
 
 theta = np.array((1,1))
-ones = np.ones((1, X_training.shape[0]))
-X_training = X_training.transpose()
+ones = np.ones((1, X_training_raw.shape[0]))
+X_training = X_training_raw.transpose()
 X_training = np.vstack((ones, X_training))
 y_training = y_training.reshape((1, len(y_training)))
 
-ones = np.ones((1, X_val.shape[0]))
-X_val = X_val.transpose()
+ones = np.ones((1, X_val_raw.shape[0]))
+X_val = X_val_raw.transpose()
 X_val = np.vstack((ones, X_val))
 y_val = y_val.reshape((1, len(y_val)))
 
@@ -75,4 +75,43 @@ plt.figure(2)
 plt.plot(error_training, label = 'Training error')
 plt.plot(error_cross_val, label = 'Cross validation error')
 plt.grid()
+
+
+lambda_for_regularization = 0
+
+X_poly_training = linearregression.get_polynomial_feature_matrix_for_univariate_feature_matrix(X_training_raw.transpose(), 8)
+X_poly_training = linearregression.normalized_features_matrix(X_poly_training)
+ones = np.ones((1, X_poly_training.shape[1]))
+X_poly_training = np.vstack((ones, X_poly_training))
+
+X_poly_val = linearregression.get_polynomial_feature_matrix_for_univariate_feature_matrix(X_val_raw.transpose(), 8)
+X_poly_val = linearregression.normalized_features_matrix(X_poly_val)
+ones = np.ones((1, X_poly_val.shape[1]))
+X_poly_val = np.vstack((ones, X_poly_val))
+
+initial_theta = np.zeros((X_poly_training.shape[0]))
+
+optimized_theta = linearregression.minimize_cost_and_find_theta_with_regularization(initial_theta,
+                                                                                    X_poly_training,
+                                                                                    y_training,
+                                                                                    lambda_for_regularization,
+                                                                                    OptimizationAlgo.FMIN_CG)
+
+prediction_poly = optimized_theta.dot(X_poly_training)
+
+plt.figure(1)
+plt.scatter(X_training[1], prediction_poly)
+
+
+error_training_poly, error_cross_val_poly = diagnostics.get_training_error(X_poly_training,
+                                                                 y_training,
+                                                                 X_poly_val,
+                                                                 y_val)
+
+plt.figure(2)
+plt.plot(error_training_poly, label = 'Training error with polynomial regression')
+plt.plot(error_cross_val_poly, label = 'Cross validation error with polynomial regression')
+plt.grid()
+plt.legend()
 plt.show()
+
